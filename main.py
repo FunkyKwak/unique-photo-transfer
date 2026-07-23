fe22e4e9ac30c25d71598bc2ffbb1d7c11c6e1c4
Yann <y.faure@groupe-esigelec.fr>
2026-07-23 12:26:06 +0200

    Icone

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import sys
 from PySide6.QtWidgets import QApplication
+from PySide6.QtGui import QIcon
 
 from ui import MainWindow
 
 
 def main():
     app = QApplication(sys.argv)
-
+    app.setWindowIcon(QIcon("assets/unique-photo-transfer.ico"))
+    
     window = MainWindow()
     window.show()
 
